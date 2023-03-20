2553abbef5f6da82040b741b15f4ef7b828df5d9
Jess Lawson <52499660+abysmal-jess@users.noreply.github.com>
2023-03-20 10:31:32 +0000

    ✨Init.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <a href="https://jess.abysmal.dev">
+          jess.abysmal.dev
+        </a>
       </header>
     </div>
   );
